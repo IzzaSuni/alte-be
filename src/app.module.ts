e61a20792d65990c8ba75773cf308d6b5b41b8db
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2023-01-03 03:21:41 +0700

    okee

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MailerModule } from '@nestjs-modules/mailer';
-
+ 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://soxiSpeed:Sunariadi180901!@alte.uypme2k.mongodb.net/ALTE?retryWrites=true&w=majority',
     ),
     UserModule,
-    MailerModule.forRoot({
-      transport: {
-        host: 'gmail',
-        auth: {
-          user: 'kmitera.cms@gmail.com',
-          pass:
-            'Sunariadi180901',
-        },
-      },
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
