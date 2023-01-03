f7bfb0734e72498c8f32bfc5c76929c97ff88ad2
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2023-01-03 12:32:02 +0700

    ok

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    // console.log(v8.getHeapStatistics().heap_size_limit / (1024 * 1024));
+
     return 'Hello World!';
   }
 }
